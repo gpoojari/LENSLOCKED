2229f10798a8fa9646569a2ad209745389da8f9b
Girish Poojari <poojari.girish@gmail.com>
2022-08-24 20:47:20 +0100

    just update 1

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/gpoojari/lenslocked
 
 go 1.18
 
-require github.com/go-chi/chi/v5 v5.0.7 // indirect
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/chi/v5 v5.0.7
+)
--- main.go
+++ main.go
@@ -3,17 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "<h1>Welcome to my Awesome site!</h1>")
+	id := chi.URLParam(r, "id")
+	fmt.Fprintf(w, "hi %v", id)
 
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch , email me at <a href=\"mailto:poojari.girish@gmail.com\">Girish Poojari</a>.")
+	id := chi.URLParam(r, "id")
+	fmt.Fprintf(w, "hi %v", id)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
@@ -75,7 +82,16 @@ func main() {
 	//http.HandleFunc("/", homeHandler)
 	//http.HandleFunc("/contact", contactHandler)
 	//http.HandleFunc("/", pathHandler)
-	var router Router
+	//var router Router
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+
 	fmt.Println("Starting the server on :3000....")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }
